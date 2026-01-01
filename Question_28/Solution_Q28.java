43cb633e517bd406d18c3c9d4149c1f0b22cf0fc
Aarav Goyal <gl.aarav@gmail.com>
2026-01-01 10:17:44 -0800

    Add Completed Q28 with

---
+++ Question_28/Solution_Q28.java
@@ -0,0 +1,21 @@
+/**
+ * This class provides a solution to the problem of finding the index of the
+ * first occurrence of a substring in a string.
+ *
+ * @author Aarav Goyal
+ * @since 1/1/2025
+ */
+public class Solution_Q28 {
+    /**
+     * Finds the index of the first occurrence of the substring `needle` in the
+     * string `haystack`.
+     *
+     * @param haystack the string to search within
+     * @param needle   the substring to search for
+     * @return the index of the first occurrence of `needle` in `haystack`, or -1 if
+     *         `needle` is not found
+     */
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
